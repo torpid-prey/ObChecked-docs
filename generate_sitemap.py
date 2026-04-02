a748c460e6ae5cd489def5c91fd5ed8aa6dc3781
Chloe Garcia <torpid.prey@hotmail.com>
2026-04-02 13:48:23 +1100

    another fix to sitemap

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -72,6 +72,9 @@ def collect_urls() -> list[tuple[str, str]]:
 
 def write_sitemap(urls: list[tuple[str, str]]) -> None:
     lines = [
+        '---',
+        'layout: null',
+        '---',
         '<?xml version="1.0" encoding="UTF-8"?>',
         '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
     ]
